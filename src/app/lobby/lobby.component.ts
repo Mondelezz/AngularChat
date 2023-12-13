c02aec580ebaa9ee585466a0967db3ea3e095284
Mondelezz <pankov.egor26032005@yandex.ru>
2023-12-13 22:55:43 +0300

    лобби

---
+++ src/app/lobby/lobby.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-lobby',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './lobby.component.html',
+  styleUrl: './lobby.component.scss'
+})
+export class LobbyComponent {
+
+}
